e8cce530e3025f48a99f29a6c8e337f36bd9fa2c
IgorSwatowski <igi66@wp.pl>
2022-11-21 15:27:49 +0100

    Fix localStorage

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -19,7 +19,7 @@ export const AuthContextProvider = ({ children }: any) => {
     })
 
     useEffect(() => {
-      const user = JSON.parse(localStorage.getItem('user')!)
+      const user = JSON.parse(localStorage.getItem('userInfo')!)
   
       if (user) {
         dispatch({ type: 'LOGIN', payload: user }) 
